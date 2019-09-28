4fe809dc2a37ae67eb1711d82c7e6d3bb60562b4
Andrew Williams <1williamsaj@gmail.com>
2019-09-28 20:53:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,7 +5,14 @@ class TicTacToe
   end
 
   WIN_COMBINATIONS = [
-    
+    [0,1,2], #0 'top row win'
+    [3,4,5], #1 'middle row win'
+    [6,7,8], #2 'bottom row win'
+    [0,3,6], #3 'first column win'
+    [1,4,7], #4 'second column win'
+    [2,5,8], #5 'third column win'
+    [0,4,8], #6 'left diagonal win'
+    [2,4,6]  #7 'right diagonal win'
   ]
 
 end
